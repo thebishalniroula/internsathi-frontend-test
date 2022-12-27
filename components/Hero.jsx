25484b8449945475c8c3e8a354fe3597972c9d7b
Bishal Niroula <niroulabishal199@gmail.com>
2022-12-27 11:13:12 +0545

    ..

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import image from "/internet-pasal-hero.png";
 import "./Hero.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
@@ -7,26 +6,28 @@ import { faPlayCircle } from "@fortawesome/free-solid-svg-icons";
 
 const Hero = () => {
   return (
-    <div className="hero">
-      <div className="left">
-        <h1>The Nepali Commerce Platform</h1>
-        <p>
-          Build your business with InternetPasal to sell online, offline, and
-          everywhere in between.
-        </p>
-        <div className="buttons">
-          <button className="main-btn">
-            Try for free{` `}
-            <FontAwesomeIcon icon={faChevronRight} />
-          </button>
-          <button className="sec-btn">
-            <FontAwesomeIcon icon={faPlayCircle} />
-            {` `} Watch the video
-          </button>
+    <div className="container">
+      <div className="hero">
+        <div className="left">
+          <h1>The Nepali Commerce Platform</h1>
+          <p>
+            Build your business with InternetPasal to sell online, offline, and
+            everywhere in between.
+          </p>
+          <div className="buttons">
+            <button className="main-btn">
+              Try for free{` `}
+              <FontAwesomeIcon icon={faChevronRight} />
+            </button>
+            <button className="sec-btn">
+              <FontAwesomeIcon icon={faPlayCircle} />
+              {` `} Watch the video
+            </button>
+          </div>
         </div>
-      </div>
-      <div className="right">
-        <img src={image} alt="" />
+        <div className="right">
+          <img src={"/internet-pasal-hero.png"} alt="" />
+        </div>
       </div>
     </div>
   );
